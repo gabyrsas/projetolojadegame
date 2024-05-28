6590a13f2bfdf1f203e1f57e0883ed483ecb009e
gaby_rosa <165588697+gabyrsas@users.noreply.github.com>
2024-05-28 09:17:37 -0300

    projeto loja de game

---
+++ src/main/java/com/generation/lojadejogos/repository/CategoriaRepository.java
@@ -0,0 +1,17 @@
+package com.generation.lojadejogos.repository;
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.lojadejogos.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
+}
+
